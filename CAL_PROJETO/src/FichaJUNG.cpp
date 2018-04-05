bcbe3533224895e8a37f06c79de62d0df94b82e3
angel <angel@172.30.23.249>
2018-04-05 15:32:30 +0100

    Added stores locations+ path

--- CAL_PROJETO/src/FichaJUNG.cpp
+++ CAL_PROJETO/src/FichaJUNG.cpp
@@ -3,19 +3,26 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include "Grafos.h"
+#include "Node.h"
+
 
 void graphviewer_display();
 
+void print_path_GV(vector<Node> path,GraphViewer * gv);
 
 
 void graphviewer_display()
 {
-	GraphViewer *gv = new GraphViewer(8, 8, false);
-
+	GraphViewer *gv = new GraphViewer(800, 800, false);
 	gv->createWindow(800,800);
-
 	gv->defineEdgeColor("blue");
 	gv->defineVertexColor("yellow");
+	gv->defineEdgeCurved(false);
+
+
+
 
 	ifstream inFile;
 
@@ -23,23 +30,27 @@ void graphviewer_display()
 	inFile.open("nodes.txt");
 
 	if (!inFile) {
-	    cerr << "Unable to open file datafile.txt";
+	    cerr << "Unable to open file nodes.txt";
 	    exit(1);   // call system to stop
 	}
 
-	std::string   line,line2;
+	std::string   line,line2,line3;
 
 	int idNo=0;
 	int X=0;
 	int Y=0;
+	int idStore=0;
+	string a="";
 
+
+	string letra;
 	while(std::getline(inFile, line))
 	{
+
 	    std::stringstream linestream(line);
 	    std::string         data;
 
 	    linestream >> idNo;
-
 	    std::getline(linestream, data, ';');  // read up-to the first ; (discard ;).
 	    linestream >> X;
 	    std::getline(linestream, data, ';');  // read up-to the first ; (discard ;).
@@ -47,6 +58,27 @@ void graphviewer_display()
 	    std::getline(linestream, data, ';');  // read up-to the first ; (discard ;).
 	    gv->addNode(idNo,Y,X);//-----------------------------------------------------------------------
 
+	    ifstream store;
+	   	store.open("pontosPartilha.txt");
+	   	if (!store)
+	   	{
+	   		cerr << "Unable to open file pontosPartilha.txt";
+	   		exit(1);   // call system to stop
+	   	}
+
+	   	while(std::getline(store, line3))
+	   	{
+	   		std::stringstream linestream3(line3);
+
+	   	 	std::getline(linestream3, data, '/');  // read up-to the first ; (discard ;).
+	   	 	linestream3 >> idStore;
+	   	    std::getline(linestream3, data, '/');
+	   	 	if(idStore==idNo)
+	   	 	{
+	   	 	    gv->setVertexColor(idNo,"green");
+	   	   	}
+	   	}
+	   	store.close();
 	}
 
 	inFile.close();
@@ -83,7 +115,7 @@ void graphviewer_display()
 		    linestream >> idNoDestino;
 		    ifstream street;
 		    street.open("roads.txt");
-		    if (!inFile) {
+		    if (!street) {
 		    		    cerr << "Unable to open file datafile.txt";
 		    		    exit(1);   // call system to stop
 		    		}
@@ -108,5 +140,14 @@ void graphviewer_display()
 
 		inFile.close();
 
+
 	gv->rearrange();
+}
+
+void print_path_GV(vector<Node> path,GraphViewer * gv)
+{
+	for(auto const &v: path)
+	{
+		gv->setVertexColor(v.getID(), RED);
+	}
 }
